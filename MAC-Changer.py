197697bfe2055bb2f4b07aeff1003a0cadb1f128
ARJUN S <arjunevershine@gmail.com>
2022-03-12 17:40:28 +0530

    fix 1

--- MAC-Changer.py
+++ MAC-Changer.py
@@ -6,9 +6,9 @@ def SelectInterface(choice):
     interfaces = {1:'eth0',2:'wlan0'}
     new_mac = input("Enter desired MAC addreess (Default: 00:11:22:33:44:55): ") or '00:11:22:33:44:55'
     print("[+] Changing MAC Address to "+new_mac+" .....")
-    subprocess.call("ifconfig "+interfaces.get(choice)+"down",shell=True)
-    subprocess.call("ifconfig "+interfaces.get(choice)+"hw ether "+new_mac,shell=True)
-    subprocess.call("ifconfig "+interfaces.get(choice)+"up",shell=True)
+    subprocess.call("ifconfig "+interfaces.get(choice)+" down",shell=True)
+    subprocess.call("ifconfig "+interfaces.get(choice)+" hw ether "+new_mac,shell=True)
+    subprocess.call("ifconfig "+interfaces.get(choice)+" up",shell=True)
 
 
 subprocess.call("netstat -i", shell=True)
