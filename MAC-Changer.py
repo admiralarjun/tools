6ca0a423bcc12263386b4728de95c9bd541f8bb6
ARJUN S <arjunevershine@gmail.com>
2022-03-12 18:15:41 +0530

    Updated Banner

--- MAC-Changer.py
+++ MAC-Changer.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
 
+title = '''
+  __  __          _____       _____ _    _          _   _  _____ ______ _____  
+ |  \/  |   /\   / ____|     / ____| |  | |   /\   | \ | |/ ____|  ____|  __ \ 
+ | \  / |  /  \ | |   ______| |    | |__| |  /  \  |  \| | |  __| |__  | |__) |
+ | |\/| | / /\ \| |  |______| |    |  __  | / /\ \ | . ` | | |_ |  __| |  _  / 
+ | |  | |/ ____ \ |____     | |____| |  | |/ ____ \| |\  | |__| | |____| | \ \ 
+ |_|  |_/_/    \_\_____|     \_____|_|  |_/_/    \_\_| \_|\_____|______|_|  \_\
+                                                                               
+'''
+
 import subprocess
 
 def SelectInterface(choice):
     interfaces = {1:'eth0',2:'wlan0'}
     new_mac = input("Enter desired MAC addreess (Default: 00:11:22:33:44:55): ") or '00:11:22:33:44:55'
     print("[+] Changing MAC Address to "+new_mac+" .....")
+    subprocess.call("ifconfig "+interfaces.get(choice)+" down",shell=True)
     subprocess.call("ifconfig "+interfaces.get(choice)+" down",shell=True)
     subprocess.call("ifconfig "+interfaces.get(choice)+" hw ether "+new_mac,shell=True)
     subprocess.call("ifconfig "+interfaces.get(choice)+" up",shell=True)
