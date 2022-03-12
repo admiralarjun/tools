cd8f32714a270a92bb77a411f96628e2b8a07bf5
ARJUN S <arjunevershine@gmail.com>
2022-03-12 18:22:42 +0530

    Recovery option

--- MAC-Changer.py
+++ MAC-Changer.py
@@ -7,7 +7,8 @@ title = '''
  | |\/| | / /\ \| |  |______| |    |  __  | / /\ \ | . ` | | |_ |  __| |  _  / 
  | |  | |/ ____ \ |____     | |____| |  | |/ ____ \| |\  | |__| | |____| | \ \ 
  |_|  |_/_/    \_\_____|     \_____|_|  |_/_/    \_\_| \_|\_____|______|_|  \_\
-                                                                               
+
+[+] WARNING: Execute this at your own risk.                                                                               
 '''
 
 import subprocess
@@ -16,7 +17,7 @@ def SelectInterface(choice):
     interfaces = {1:'eth0',2:'wlan0'}
     new_mac = input("Enter desired MAC addreess (Default: 00:11:22:33:44:55): ") or '00:11:22:33:44:55'
     print("[+] Changing MAC Address to "+new_mac+" .....")
-    subprocess.call("ifconfig "+interfaces.get(choice)+" down",shell=True)
+    subprocess.call("ifconfig >> config.txt",shell=True)
     subprocess.call("ifconfig "+interfaces.get(choice)+" down",shell=True)
     subprocess.call("ifconfig "+interfaces.get(choice)+" hw ether "+new_mac,shell=True)
     subprocess.call("ifconfig "+interfaces.get(choice)+" up",shell=True)
